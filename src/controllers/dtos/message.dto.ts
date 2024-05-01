b1473df83e07b819eb0e2068787d74472681e00d
Ilya Dolmatov <ilya@guardianvets.com>
2024-05-01 19:06:54 +0300

    MF-34 create dtos

---
+++ src/controllers/dtos/message.dto.ts
@@ -0,0 +1,11 @@
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { MessageMode } from '../../core';
+
+export class CreateMessageDto {
+  @IsString()
+  @IsNotEmpty()
+  body: string;
+
+  @IsEnum(MessageMode)
+  mode: MessageMode;
+}
