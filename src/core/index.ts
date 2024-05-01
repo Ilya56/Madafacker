58b9ba68512801b57366d14c448293a0a610b7a9
Ilya Dolmatov <ilya@guardianvets.com>
2024-05-01 16:31:48 +0300

    MF-18 add docs for all object/files/folders

---
+++ src/core/index.ts
@@ -0,0 +1,8 @@
+/**
+ * This is an application core folder.
+ * All core folders should be independent of any other folders, it's a core of all systems with only business logic.
+ * All other layers depend on core logic, but core is a logic of the real world.
+ */
+
+export * from './enums';
+export * from './entities';
