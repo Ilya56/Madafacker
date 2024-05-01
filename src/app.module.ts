6c2b9ea6d3387947886c21e144be031ee49df9c6
Ilya Dolmatov <ilya@guardianvets.com>
2024-05-01 19:22:25 +0300

    MF-34 implement user controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserUseCasesModule } from './use-cases/user/user-use-cases.module';
+import { UserController } from './controllers';
 import { FactoryModule } from './controllers';
 
 @Module({
   imports: [UserUseCasesModule, FactoryModule],
-  controllers: [],
+  controllers: [UserController],
   providers: [],
 })
 export class AppModule {}
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,8 @@
+/**
+ * It's controllers and representers folder.
+ * They're mixed here to process data in right way and to return data in right way
+ */
 
+export * from './dtos';
+export * from './factories';
+export * from './user.controller';
