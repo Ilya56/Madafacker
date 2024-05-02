63c4ceb6d67fa17c2bd3edf09ce1a44dcbb1b13a
Ilya Dolmatov <ilya@guardianvets.com>
2024-05-02 21:29:33 +0300

    MF-35 implement sequelize connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserUseCasesModule } from './use-cases/user/user-use-cases.module';
 import { UserController } from './controllers';
 import { FactoryModule } from './controllers';
+import { CustomConfigModule } from './config';
 
 @Module({
-  imports: [UserUseCasesModule, FactoryModule],
+  imports: [CustomConfigModule, UserUseCasesModule, FactoryModule],
   controllers: [UserController],
   providers: [],
 })
