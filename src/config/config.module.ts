61f1166b2571e993dab0362fd8813e3467ca340f
Ilya Dolmatov <ilya@guardianvets.com>
2024-05-02 21:28:43 +0300

    MF-35 implement project config

---
+++ src/config/config.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './configuration';
+
+/**
+ * This module exists to separate config logic/data/config from another parts of the project
+ */
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+  ],
+})
+export class CustomConfigModule {}
---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+/**
+ * This folder stores project config. It workings with dotenv package, so it's a source of the config
+ */
+
+export * from './config.module';
