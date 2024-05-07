7a5e43f8b85e9fe8d35f0dbb6f031c8be37bd031
Ilya Dolmatov <ilya@guardianvets.com>
2024-05-07 19:05:52 +0300

    MF-44 add id to the entity parent and make it abstract

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -2,4 +2,10 @@
  * Just a parent class for all entities to have one parent
  */
 
-export class Entity {}
+export abstract class Entity {
+  /**
+   * Entity unique identifier
+   * By default its string because we use uuid in the begining
+   */
+  id: string;
+}
