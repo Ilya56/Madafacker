25284ae8ef0e67fd071f09522f625153cf42d091
Ilya Dolmatov <ilya@guardianvets.com>
2024-05-08 16:11:36 +0300

    MF-37 implement user factory tests

---
+++ src/controllers/factories/test/user-factory.service.spec.ts
@@ -0,0 +1,43 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserFactoryService } from '@controllers';
+import { CreateUserDto, UpdateUserDto } from '../../dtos';
+import { User } from '@core';
+
+describe('UserFactoryService', () => {
+  let userFactoryService: UserFactoryService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserFactoryService],
+    }).compile();
+
+    userFactoryService = module.get<UserFactoryService>(UserFactoryService);
+  });
+
+  describe('createNewUser', () => {
+    it('should create a new user with the correct name from CreateUserDto', () => {
+      const createUserDto: CreateUserDto = { name: 'John Doe' };
+
+      const expectedUser = new User();
+      expectedUser.name = 'John Doe';
+
+      const result = userFactoryService.createNewUser(createUserDto);
+
+      expect(result).toBeInstanceOf(User);
+      expect(result.name).toBe(expectedUser.name);
+    });
+  });
+
+  describe('updateUser', () => {
+    it('should update a user with the correct name from UpdateUserDto', () => {
+      const updateUserDto: UpdateUserDto = { name: 'Jane Doe' };
+      const expectedUser = new User();
+      expectedUser.name = 'Jane Doe';
+
+      const result = userFactoryService.updateUser(updateUserDto);
+
+      expect(result).toBeInstanceOf(User);
+      expect(result.name).toBe(updateUserDto.name);
+    });
+  });
+});
