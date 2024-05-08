24fcf63fea8e17495654b7bef96d92ee79ad1e65
Ilya Dolmatov <ilya@guardianvets.com>
2024-05-08 17:21:51 +0300

    MF-37 implement sequelize repo tests

---
+++ src/frameworks/data-services/sequelize/entity-repositories/test/sequelize-user-repository.spec.ts
@@ -0,0 +1,62 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { SequelizeUserRepository } from '../sequelize-user-repository';
+import { UserModel } from '@frameworks/data-services/sequelize/models';
+import { User } from '@core';
+
+describe('SequelizeUserRepository', () => {
+  let userRepository: SequelizeUserRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        SequelizeUserRepository,
+        {
+          provide: UserModel,
+          useValue: {
+            update: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    userRepository = module.get<SequelizeUserRepository>(SequelizeUserRepository);
+  });
+
+  describe('updateByName', () => {
+    it('should update a user by name and return the updated user', async () => {
+      const name = 'John Doe';
+
+      const userUpdateInfo = new User();
+      userUpdateInfo.name = 'Test';
+
+      const updatedUser = { name: 'Test' };
+
+      jest.spyOn(UserModel, 'update').mockResolvedValue([1, [updatedUser]] as never as [number]);
+
+      const result = await userRepository.updateByName(name, userUpdateInfo);
+
+      expect(result).toEqual(updatedUser);
+      expect(UserModel.update).toHaveBeenCalledWith(userUpdateInfo, {
+        where: { name },
+        returning: true,
+      });
+    });
+
+    it('should return null if no user is updated', async () => {
+      const name = 'Nonexistent User';
+      const userUpdateInfo = new User();
+      userUpdateInfo.name = 'Test';
+
+      // Mock the update method to simulate Sequelize behavior
+      jest.spyOn(UserModel, 'update').mockResolvedValue([0, []] as never as [number]);
+
+      const result = await userRepository.updateByName(name, userUpdateInfo);
+
+      expect(result).toBeUndefined();
+      expect(UserModel.update).toHaveBeenCalledWith(userUpdateInfo, {
+        where: { name },
+        returning: true,
+      });
+    });
+  });
+});
