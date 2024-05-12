c7d982567813c2f564ea100b287fd3af7bb1ab0e
Ilya Dolmatov <ilya@guardianvets.com>
2024-05-12 16:41:46 +0300

    MF-35 fix message repo test

--- src/frameworks/data-services/sequelize/entity-repositories/test/sequelize-message-repository.spec.ts
+++ src/frameworks/data-services/sequelize/entity-repositories/test/sequelize-message-repository.spec.ts
@@ -2,16 +2,6 @@ import { SequelizeMessageRepository } from '../sequelize-message-repository';
 import { SequelizeGenericRepository } from '@frameworks/data-services/sequelize/sequelize-generic-repository';
 import { MessageModel } from '@frameworks/data-services/sequelize/models';
 
-jest.mock('@frameworks/data-services/sequelize', () => {
-  const actual = jest.requireActual('@frameworks/data-services/sequelize');
-  return {
-    ...actual,
-    SequelizeGenericRepository: jest.fn().mockImplementation(() => ({
-      create: jest.fn(),
-    })),
-  };
-});
-
 describe('SequelizeMessageRepository', () => {
   let repository: SequelizeMessageRepository;
   let superCreateSpy: any;
@@ -30,7 +20,6 @@ describe('SequelizeMessageRepository', () => {
   it('should set authorId and call super.create with the modified message', async () => {
     const message = {
       author: { id: 123 },
-      authorId: undefined,
     } as unknown as MessageModel;
 
     await repository.create(message);
