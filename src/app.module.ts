651dc6ceeee6295cc6035933f32e03ac6b6d7315
Ilya Dolmatov <ilya@guardianvets.com>
2024-05-16 11:17:51 +0300

    MF-50 create listeners module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ControllersModule } from '@controllers';
 import { CustomConfigModule } from '@config';
+import { ListenersModule } from '@listeners';
 
 /**
  * Main app module, start here
  */
 @Module({
-  imports: [CustomConfigModule, ControllersModule],
+  imports: [CustomConfigModule, ControllersModule, ListenersModule],
   providers: [],
 })
 export class AppModule {}
---
+++ src/listeners/listeners.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+
+/**
+ * Listeners module.
+ * Import all listener modules
+ */
+@Module({
+  imports: [],
+  providers: [],
+})
+export class ListenersModule {}
