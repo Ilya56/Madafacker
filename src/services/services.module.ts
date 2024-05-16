0aad50515e3b195aef7f52f3a5a65888a310ab4f
Ilya Dolmatov <ilya@guardianvets.com>
2024-05-16 11:01:59 +0300

    MF-50 add task services to the services module

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -3,9 +3,10 @@ import { DataServicesModule } from './data-services/data-services.module';
 import { UserServicesModule } from './user-services/user-services.module';
 import { DateServicesModule } from './date-service/date-service.module';
 import { AlgoServicesModule } from './algo-service/algo-services.module';
+import { TaskServicesModule } from './task-services/task-services.module';
 
 @Module({
-  imports: [DataServicesModule, UserServicesModule, DateServicesModule, AlgoServicesModule],
-  exports: [DataServicesModule, UserServicesModule, DateServicesModule, AlgoServicesModule],
+  imports: [DataServicesModule, UserServicesModule, DateServicesModule, AlgoServicesModule, TaskServicesModule],
+  exports: [DataServicesModule, UserServicesModule, DateServicesModule, AlgoServicesModule, TaskServicesModule],
 })
 export class ServicesModule {}
