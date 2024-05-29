b5365370f5cb540005a6a7b4c7673402b83ac1ce
Ilya Dolmatov <ilya@guardianvets.com>
2024-05-29 11:51:47 +0300

    MF-59 create redis config

--- src/frameworks/task-services/bull/bull-task-services.module.ts
+++ src/frameworks/task-services/bull/bull-task-services.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
-import { BullModule } from '@nestjs/bull';
+import { BullModule, BullRootModuleOptions } from '@nestjs/bull';
 import { TaskServiceAbstract } from '@core';
 import { BullTaskServices } from './bull-task-services.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigType } from '@config';
 
 /**
  * This module is a Bull task service implementation
  */
 @Module({
-  imports: [BullModule.forRoot({})],
+  imports: [
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService): BullRootModuleOptions => ({
+        redis: configService.get<ConfigType['redis']>('redis'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [{ provide: TaskServiceAbstract, useClass: BullTaskServices }],
   exports: [TaskServiceAbstract],
 })
