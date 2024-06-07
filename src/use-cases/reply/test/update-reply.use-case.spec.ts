713b10cd9e861c85cbb3c31a77875e31f28e5a5d
Ilya Dolmatov <ilya@guardianvets.com>
2024-06-07 14:32:16 +0300

    MF-63 create update reply use case

---
+++ src/use-cases/reply/test/update-reply.use-case.spec.ts
@@ -0,0 +1,44 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UpdateReplyUseCase } from '@use-cases/reply';
+import { DataServiceAbstract } from '@core';
+import { SERVICES_PROVIDER } from '@use-cases/test/test-helpers';
+
+describe('UpdateReplyUseCase', () => {
+  let useCase: UpdateReplyUseCase;
+  let dataService: DataServiceAbstract;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UpdateReplyUseCase, ...SERVICES_PROVIDER],
+    }).compile();
+
+    useCase = module.get<UpdateReplyUseCase>(UpdateReplyUseCase);
+    dataService = module.get<DataServiceAbstract>(DataServiceAbstract);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should update a reply with the given replyId and new reply data', async () => {
+    const reply = { id: 'reply-id', public: true } as any;
+
+    jest.spyOn(dataService.replies, 'update').mockResolvedValue(reply);
+
+    const result = await useCase.execute(reply);
+
+    expect(dataService.replies.update).toHaveBeenCalledWith(reply.id, reply);
+    expect(result).toBe(reply);
+  });
+
+  it('should return null if the reply is not found', async () => {
+    const reply = { id: 'reply-id', public: true } as any;
+
+    jest.spyOn(dataService.replies, 'update').mockResolvedValue(null);
+
+    const result = await useCase.execute(reply);
+
+    expect(dataService.replies.update).toHaveBeenCalledWith(reply.id, reply);
+    expect(result).toBeNull();
+  });
+});
