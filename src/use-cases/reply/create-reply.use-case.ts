e603ebdea571baa5acf518ba0e7f6a6b8cb68ae7
Ilya Dolmatov <ilya@guardianvets.com>
2024-06-17 21:57:59 +0300

    MF-43 refactor create reply use case on param destructor

--- src/use-cases/reply/create-reply.use-case.ts
+++ src/use-cases/reply/create-reply.use-case.ts
@@ -12,17 +12,15 @@ export class CreateReplyUseCase extends CommandAbstract<CreateReplyInput, Reply>
   /**
    * Creates a new reply from the current user to the provided message
    * If user tries to reply on the message that not exists, it throws not found error
-   * @param input.reply reply object
-   * @param input.parentId id of the message to reply
+   * @param reply reply object
+   * @param parentId id of the message to reply
    */
-  protected async implementation(input: CreateReplyInput): Promise<Reply> {
-    const reply = input.reply;
-
+  protected async implementation({ reply, parentId }: CreateReplyInput): Promise<Reply> {
     reply.author = await this.userService.getCurrentUser();
-    const parentMessage = await this.dataService.messages.getById(input.parentId);
+    const parentMessage = await this.dataService.messages.getById(parentId);
 
     if (!parentMessage) {
-      throw new NotFoundError(`Message with id ${input.parentId} not found`);
+      throw new NotFoundError(`Message with id ${parentId} not found`);
     }
 
     reply.parent = parentMessage;
