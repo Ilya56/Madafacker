9b2100941b061315f0fabe7c5d19434ba43a3bfb
Ilya Dolmatov <ilya@guardianvets.com>
2024-06-24 21:26:44 +0300

    MF-58 add migration

---
+++ src/frameworks/data-services/sequelize/migrations/20240624182401-add-was-sent-message-field.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * Adds a field wasSent to the message model with type boolean and default value false
+ * @type {import('sequelize-cli').Migration}
+ */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('MessageModels', 'wasSent', {
+      type: Sequelize.DataTypes.BOOLEAN,
+      defaultValue: false,
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeColumn('MessageModels', 'wasSent');
+  },
+};
