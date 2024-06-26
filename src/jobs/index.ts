720c62e4aa5e9c9e17fd5b419611a2d7593bd80a
Ilya Dolmatov <ilya@guardianvets.com>
2024-06-26 14:18:49 +0300

    MF-58 create jobs module

---
+++ src/jobs/index.ts
@@ -0,0 +1,5 @@
+/**
+ * This folder is created to store all jobs that are used on the project
+ */
+
+export * from './jobs.module';
---
+++ src/jobs/jobs.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+/**
+ * Jobs module.
+ * Re-export all jobs
+ */
+@Module({
+  imports: [],
+})
+export class JobsModule {}
