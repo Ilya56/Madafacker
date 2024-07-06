e4214a97f87a16d712df2fb0472bfa7fedd78724
Ilya Dolmatov <ilya@guardianvets.com>
2024-07-06 12:12:08 +0300

    MF-68 change bull queue config approach

--- src/frameworks/task-services/bull/test/bull-queue.spec.ts
+++ src/frameworks/task-services/bull/test/bull-queue.spec.ts
@@ -1,11 +1,15 @@
 import { BullQueue } from '../bull-queue';
 import Bull, { Queue } from 'bull';
+import { ConfigService } from '@nestjs/config';
+import { ConfigType } from '@config';
 
 jest.mock('bull');
+jest.mock('@nestjs/config');
 
 describe('BullQueue', () => {
   let bullQueue: BullQueue<any>;
   let queueMock: Queue<any>;
+  let configServiceMock: ConfigService;
 
   beforeEach(() => {
     // Mock the Bull Queue constructor and methods
@@ -16,8 +20,16 @@ describe('BullQueue', () => {
 
     (Bull as jest.Mock).mockImplementation(() => queueMock);
 
-    // Instantiate the BullQueue with the mocked Bull Queue
-    bullQueue = new BullQueue('testQueue');
+    // Mock ConfigService
+    configServiceMock = {
+      get: jest.fn().mockReturnValue({
+        host: 'localhost',
+        port: 6379,
+      } as ConfigType['redis']),
+    } as unknown as ConfigService;
+
+    // Instantiate the BullQueue with the mocked ConfigService and Bull Queue
+    bullQueue = new BullQueue(configServiceMock, 'testQueue');
   });
 
   afterEach(() => {
