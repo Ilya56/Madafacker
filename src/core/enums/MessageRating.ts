16ae969c4e369b8d45ffb136dab8e001d2f183af
Ilya Dolmatov <ilya@guardianvets.com>
2024-07-20 22:35:09 +0300

    MF-90 create message rating enum

---
+++ src/core/enums/MessageRating.ts
@@ -0,0 +1,8 @@
+/**
+ * Message rating is a rating from not author. Rating can give coins if it's like or better.
+ */
+export enum MessageRating {
+  dislike = 'dislike',
+  like = 'like',
+  superlike = 'superlike',
+}
