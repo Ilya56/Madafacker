3b48b1ddc748f8d1b1aaebbf918f83c02570c2d7
Ilya Dolmatov <dolmatoffilya@gmail.com>
2024-09-03 20:38:32 +0300

    MF-128 install SentryModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,8 @@
+// IMPORTANT: Make sure to import `instrument.js` at the top of your file.
+import './instrument';
+
 import { Module } from '@nestjs/common';
+import { MonitorServiceModule } from './services/monitor-service/monitor-service.module';
 import { ControllersModule } from '@controllers';
 import { CustomConfigModule } from '@config';
 import { ListenersModule } from '@listeners';
@@ -8,7 +12,7 @@ import { ListenersModule } from '@listeners';
  */
 @Module({
   // listeners should be before controllers
-  imports: [CustomConfigModule, ListenersModule, ControllersModule],
+  imports: [MonitorServiceModule, CustomConfigModule, ListenersModule, ControllersModule],
   providers: [],
 })
 export class AppModule {}
