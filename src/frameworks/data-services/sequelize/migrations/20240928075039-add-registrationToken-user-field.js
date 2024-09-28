151770684764585a725c74d12b3230bdf38ae068
Ilya Dolmatov <dolmatoffilya@gmail.com>
2024-09-28 11:17:50 +0300

    MF-140 implement user registrationToken in data service

---
+++ src/frameworks/data-services/sequelize/migrations/20240928075039-add-registrationToken-user-field.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * Adds a field coins to the user model with type number and default value 0
+ * @type {import('sequelize-cli').Migration}
+ */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('UserModels', 'registrationToken', {
+      type: Sequelize.STRING,
+      allowNull: true,
+      unique: true,
+    });
+
+    await queryInterface.sequelize.query(`
+      UPDATE "UserModels" SET "registrationToken" = uuid_generate_v4() WHERE "registrationToken" IS NULL;
+    `);
+
+    await queryInterface.changeColumn('UserModels', 'registrationToken', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeColumn('UserModels', 'registrationToken');
+  },
+};
