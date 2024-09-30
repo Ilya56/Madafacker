16304816d0b0b8746a88478bd639e5278b3a3655
Ilya Dolmatov <dolmatoffilya@gmail.com>
2024-09-30 23:45:03 +0300

    MF-150 create verifyToken notify service method

--- src/core/absctract/notify-service.abstract.ts
+++ src/core/absctract/notify-service.abstract.ts
@@ -8,4 +8,11 @@ export abstract class NotifyServiceAbstract {
    * @param message message to send it
    */
   abstract notify(token: string, message: string): Promise<void>;
+
+  /**
+   * Checks that token is valid for the current implementation.
+   * Returns true is token is valid
+   * @param token token to check
+   */
+  abstract verifyToken(token: string): Promise<boolean> | boolean;
 }
