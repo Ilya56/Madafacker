a55cb043b953d42056de6b297fdcf43282d72915
Ilya Dolmatov <dolmatoffilya@gmail.com>
2024-10-07 12:56:58 +0300

    MF-128 move instrument initialization on the very beginning

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,3 @@
-// IMPORTANT: Make sure to import `instrument.js` at the top of your file.
-import './instrument';
-
 import { Module } from '@nestjs/common';
 import { MonitorServiceModule } from './services/monitor-service/monitor-service.module';
 import { ControllersModule } from '@controllers';
