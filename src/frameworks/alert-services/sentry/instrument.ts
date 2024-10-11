f9fc701447fefe8ed9a245605403b44684647c3a
Ilya Dolmatov <dolmatoffilya@gmail.com>
2024-10-11 20:50:53 +0300

    MF-128 update sentry config

--- src/frameworks/alert-services/sentry/instrument.ts
+++ src/frameworks/alert-services/sentry/instrument.ts
@@ -2,4 +2,8 @@ import * as Sentry from '@sentry/nestjs';
 
 Sentry.init({
   dsn: process.env.SENTRY_DNS,
+  tracePropagationTargets: [],
+  // adapter on the Vercel
+  environment: process.env.VERCEL_ENV || 'dev',
+  skipOpenTelemetrySetup: true,
 });
