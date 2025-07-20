dc5f947650382748b4cc684665556d520ba2880c
Ilya Dolmatov <dolmatoffilya@gmail.com>
2025-07-20 13:39:57 +0300

    MF-162 create firebase auth mock

--- src/frameworks/firebase-module/index.ts
+++ src/frameworks/firebase-module/index.ts
@@ -3,3 +3,4 @@
  */
 
 export * from './firebase.module';
+export * from './mocks';
--- src/frameworks/firebase-module/mocks/index.ts
+++ src/frameworks/firebase-module/mocks/index.ts
@@ -4,3 +4,4 @@
  */
 
 export * from './firebase-admin.mock';
+export { VALID_TOKEN, REVOKED_TOKEN, USER_ID, NO_USER_TOKEN } from './firebase-admin-auth.mock';
