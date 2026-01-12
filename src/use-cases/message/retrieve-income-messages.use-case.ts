dbf7735e0d8eb891edbe161003d6f616e688a095
Ilya Dolmatov <dolmatoffilya@gmail.com>
2026-01-12 19:38:22 +0200

    feat: update use case to add rating to messages

--- src/use-cases/message/retrieve-income-messages.use-case.ts
+++ src/use-cases/message/retrieve-income-messages.use-case.ts
@@ -9,7 +9,27 @@ export class RetrieveIncomeMessagesUseCase extends QueryAbstract<void, Message[]
    * @protected
    */
   protected async implementation(): Promise<Message[]> {
-    const currentUser = await this.userService.getCurrentUser({ withIncomingMessages: true });
-    return currentUser.incomeMessages;
+    const currentUser = await this.userService.getCurrentUser({
+      withIncomingMessages: true,
+    });
+
+    const messages = currentUser.incomeMessages;
+    if (!messages || messages.length === 0) {
+      return [];
+    }
+
+    const messageIds = messages.map((message) => message.id);
+
+    const [stats, ownRatings] = await Promise.all([
+      this.dataService.incomeUserMessage.getRatingStatsByMessageIds(messageIds),
+      this.dataService.incomeUserMessage.getUserRatingsByMessageIds(currentUser.id, messageIds),
+    ]);
+
+    messages.forEach((message) => {
+      message.ratingStats = stats[message.id];
+      message.ownRating = ownRatings[message.id];
+    });
+
+    return messages;
   }
 }
